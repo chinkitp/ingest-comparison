4b65e074e069cee8689a4c98107087a175d22169
Chinkit Patel <chinkit.patel@unisa.edu.au>
2018-05-14 08:54:49 +0930

    1) es role working 2) setup for postgres

--- ansible/stack-client.sh
+++ ansible/stack-client.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ssh root@104.131.70.203 << EOF
+docker rm $(docker ps -aq) -f
+rm ingest-comparison -R
 git clone https://github.com/chinkitp/ingest-comparison.git
-cd ingest-comparison/src/clients/$2
+cd ingest-comparison/src/clients/$1
 docker-compose build --no-cache
 docker-compose up
 EOF
