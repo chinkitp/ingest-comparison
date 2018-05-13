3956757cea168518807d8bbffee900852a79f1f3
Brian Matindi <brianmatindi12@gmail.com>
2018-05-13 18:27:03 +0930

    run  client docker-compose

--- ansible/ingest-nodes.sh
+++ ansible/ingest-nodes.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
-echo $1
+
 
 if [[ $1 == 1 ]]; then
     ansible-playbook digitalocean.yml --ask-vault-pass
 fi
+
+# for (( c=1; c<=$1; c++ ))
+# do  
+#     sed -i '/masterdroplet:/ a\     - droplet$c\n' ./digitalocean.yml
+# done
+
+
+ssh root@104.131.70.203 << EOF
+git clone https://github.com/chinkitp/ingest-comparison.git
+cd ingest-comparison/src/clients/$2
+docker-compose up
+EOF
