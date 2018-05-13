c5235f3b15692ad17d468cab2419856f12e9f213
Brian Matindi <brianmatindi12@gmail.com>
2018-05-13 21:01:13 +0930

    new docker machines

--- ansible/ingest-nodes.sh
+++ ansible/ingest-nodes.sh
@@ -14,5 +14,6 @@ fi
 ssh root@104.131.70.203 << EOF
 git clone https://github.com/chinkitp/ingest-comparison.git
 cd ingest-comparison/src/clients/$2
+docker-compose build --no-cache
 docker-compose up
 EOF
