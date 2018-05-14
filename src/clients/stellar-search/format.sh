bede167bd276d0985e925afa9cad690bb5270992
Chinkit Patel <chinkit.patel@unisa.edu.au>
2018-05-14 22:30:06 +0930

    found another issue with loading of the edges

--- src/clients/stellar-search/format.sh
+++ src/clients/stellar-search/format.sh
@@ -23,22 +23,32 @@ sed -e 's/"props"/"data"/g' --in-place *.json
 cd ~/sx-stackoverflow/step2/edges.json/label=asked
 sed -e 's/"graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]/"meta":{"label":"asked","graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]}/g' --in-place *.json
 sed -e 's/"props"/"data"/g' --in-place *.json
+sed -e 's/"src"/"source"/g' --in-place *.json
+sed -e 's/"dst"/"target"/g' --in-place *.json
 
 cd ~/sx-stackoverflow/step2/edges.json/label=gave
 sed -e 's/"graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]/"meta":{"label":"gave","graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]}/g' --in-place *.json
 sed -e 's/"props"/"data"/g' --in-place *.json
+sed -e 's/"src"/"source"/g' --in-place *.json
+sed -e 's/"dst"/"target"/g' --in-place *.json
 
 cd ~/sx-stackoverflow/step2/edges.json/label=made
 sed -e 's/"graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]/"meta":{"label":"made","graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]}/g' --in-place *.json
 sed -e 's/"props"/"data"/g' --in-place *.json
+sed -e 's/"src"/"source"/g' --in-place *.json
+sed -e 's/"dst"/"target"/g' --in-place *.json
 
 cd ~/sx-stackoverflow/step2/edges.json/label=on
 sed -e 's/"graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]/"meta":{"label":"on","graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]}/g' --in-place *.json
 sed -e 's/"props"/"data"/g' --in-place *.json
+sed -e 's/"src"/"source"/g' --in-place *.json
+sed -e 's/"dst"/"target"/g' --in-place *.json
 
 cd ~/sx-stackoverflow/step2/edges.json/label=to
 sed -e 's/"graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]/"meta":{"label":"to","graphs":\["Lr41MAakQRqPwVuj3j4jCg"\]}/g' --in-place *.json
 sed -e 's/"props"/"data"/g' --in-place *.json
+sed -e 's/"src"/"source"/g' --in-place *.json
+sed -e 's/"dst"/"target"/g' --in-place *.json
 
 cat ~/sx-stackoverflow/step2/nodes.json/*/*.json > ~/sx-stackoverflow/step2/nodes.json/all-nodes.json
 cat ~/sx-stackoverflow/step2/edges.json/*/*.json > ~/sx-stackoverflow/step2/edges.json/all-edges.json
