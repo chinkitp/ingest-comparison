3fd4fe1898e9a6bd9ae6e3d70fb3ff871d4b2db1
Brian Matindi <brianmatindi12@gmail.com>
2018-05-14 18:57:23 +0930

    working master and data nodes

--- generatehosts.py
+++
@@ -1,9 +0,0 @@
-import requests, json
-r=requests.get("https://api.digitalocean.com/v2/droplets", headers={"Authorization":"Bearer b752ff24abd52999fba93e0ddc8c8a5f7e39eec58967c3dad82a6b800a2074ae"})
-
-hosts = r.json()
-
-print len(hosts['droplets'])
-
-for()
-
