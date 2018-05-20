0a23cab3aef3d1b17847df01a31813bc7206a1b7
Brian Matindi <brianmatindi12@gmail.com>
2018-05-20 17:44:16 +0930

    json datatype database change

--- src/servers/postgres/SqlScripts/ddl.sql
+++ src/servers/postgres/SqlScripts/ddl.sql
@@ -7,11 +7,11 @@ CREATE DATABASE graph;
 CREATE TABLE allnodes
 (
     ID CHAR(50)     NOT NULL,
-    DATA  jsonb  NOT NULL
+    DATA  json  NOT NULL
 );
 
 CREATE TABLE alledges
 (
     ID CHAR(50)     NOT NULL,
-    DATA  jsonb  NOT NULL
+    DATA  json  NOT NULL
 );
