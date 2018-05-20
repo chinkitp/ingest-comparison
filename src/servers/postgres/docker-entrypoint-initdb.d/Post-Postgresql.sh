507fa30e584e425dcffc776901078d11eff0e5eb
Chinkit Patel <chinkit.patel@unisa.edu.au>
2018-05-20 16:39:53 +0930

    adding a timer

--- src/servers/postgres/docker-entrypoint-initdb.d/Post-Postgresql.sh
+++ src/servers/postgres/docker-entrypoint-initdb.d/Post-Postgresql.sh
@@ -5,5 +5,5 @@ set -e
 echo "Running DDL scripts..."
 psql -h localhost -U postgres -f ../SqlScripts/ddl.sql
 echo "Loading csv files into postgresql..."
-jq -r '"\"(.id)\"|\(.)"' /postgresfiles/all-nodes.json  | psql -U postgres -h localhost  -d graph -c "COPY allnodes (id,data) FROM stdin delimiter '|' "
-jq -r '"\"(.id)\"|\(.)"' /postgresfiles/all-edges.json  | psql -U postgres -h localhost  -d graph -c "COPY alledges (id,data) FROM stdin delimiter '|' "
+time jq -r '"\"(.id)\"|\(.)"' /postgresfiles/all-nodes.json  | psql -U postgres -h localhost  -d graph -c "COPY allnodes (id,data) FROM stdin delimiter '|' "
+time jq -r '"\"(.id)\"|\(.)"' /postgresfiles/all-edges.json  | psql -U postgres -h localhost  -d graph -c "COPY alledges (id,data) FROM stdin delimiter '|' "
