727082e073511b12f0748ba259ab08161db67795
Brian Matindi <brianmatindi12@gmail.com>
2018-05-20 19:32:43 +0930

    postgres test

--- src/servers/postgres/docker-entrypoint-initdb.d/Post-Postgresql.sh
+++ src/servers/postgres/docker-entrypoint-initdb.d/Post-Postgresql.sh
@@ -8,4 +8,17 @@ time jq -r '"\"(.id)\"|\(.)"' /postgresfiles/all-edges.json  | psql -U postgres 
 
 psql -U postgres -h localhost  -d graph -c  "CREATE INDEX sourceidx ON alledges ((data ->> 'source'))"
 psql -U postgres -h localhost  -d graph -c  "CREATE INDEX targetidx ON alledges ((data ->> 'target'))"
-psql -U postgres -h localhost  -d graph -c  "SELECT * FROM alledges WHERE data ->> 'source' = 'eOmVOGSyMJC01AKsIXQxyg';"
+psql -U postgres -h localhost  -d graph -c "SELECT * FROM alledges WHERE data ->> 'source' = 'k1HyHC4vSNCq2wuTQvjV1g';"
+
+
+
+awk '{print "SELECT * FROM alledges WHERE data ->> '"'"'source'"'"' = '"'"'" $0 "'"'"';"}' '/docker-entrypoint-initdb.d/NodesIds' > 'output'
+while read -r line
+do
+     final="$line$final"
+done < output
+
+time psql -U postgres -h localhost  -d graph << EOF
+$final
+EOF
+
